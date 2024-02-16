3f457a063862832f5cca40de1ba613a43e256dab
richie <richie@ardhika.com>
2024-02-17 01:03:22 +0530

    Added implicit animation

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -32,7 +32,16 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.6, end: 1).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(isFavorite ? Icons.star : Icons.star_border, key: ValueKey(isFavorite),),
+            ),
           ),
         ],
         title: Text(meal.title),
